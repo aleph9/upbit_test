bc7e47604b2981177ff7cab02abf9606ca1000ff
aleph9 <104629654+aleph9@users.noreply.github.com>
2022-05-12 21:10:49 +0900

    Add files via upload

---
+++ bestk.py
@@ -0,0 +1,20 @@
+import pyupbit
+import numpy as np
+# 가장 좋은 k 값 구하는 법 실행 해볼것. (수수료 미포함)
+
+def get_ror(k=0.5):
+    df = pyupbit.get_ohlcv("KRW-BTC", count=7)
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
+
+
+for k in np.arange(0.1, 1.0, 0.1):
+    ror = get_ror(k)
+    print("%.1f %f" % (k, ror))
